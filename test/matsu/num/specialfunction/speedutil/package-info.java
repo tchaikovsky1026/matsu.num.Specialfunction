e35ca24a9ec050fb34c86c35c6247ec042782d9f
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-07-05 12:02:12 +0900

    test: 計算時間の計測を行うためのユーティリティ

---
+++ test/matsu/num/specialfunction/speedutil/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+/**
+ * 計算時間計測に関するユーティリティを扱うパッケージ.
+ */
+package matsu.num.specialfunction.speedutil;
