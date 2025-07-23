7f624d9ded482d90d74b0a5f25e952a93c9ba207
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-07-23 23:35:38 +0900

    refactor: 連分数をベースとする不完全ガンマ関数の計算を別のクラスに分離した

--- src/matsu/num/specialfunction/icgamma/ICGammaAtMiddleParam.java
+++ src/matsu/num/specialfunction/icgamma/ICGammaAtMiddleParam.java
@@ -5,11 +5,10 @@
  * http://opensource.org/licenses/mit-license.php
  */
 /*
- * 2025.7.6
+ * 2025.7.23
  */
 package matsu.num.specialfunction.icgamma;
 
-import matsu.num.specialfunction.GammaFunction;
 import matsu.num.specialfunction.common.Exponentiation;
 
 /**
@@ -25,8 +24,6 @@ import matsu.num.specialfunction.common.Exponentiation;
  */
 final class ICGammaAtMiddleParam extends SkeletalICGamma {
 
-    private static final double HALF_LN2PI = 0.5 * Math.log(2 * Math.PI);
-
     private final double sqrtA;
 
     /*
@@ -37,22 +34,7 @@ final class ICGammaAtMiddleParam extends SkeletalICGamma {
     private final double xLowerThreshold;
     private final double xUpperThreshold;
 
-    /*
-     * x^a e^{-x}/(aΓ(a))を計算するとき,
-     * 直接的計算では対数でa log(a)のオーダーの値が生じるが, 計算結果はa程度になる.
-     * これはすなわち, 丸め誤差が怖いことになる.
-     * したがって, スターリング近似により或る程度式変形をしておいて, xを代入して計算するようにする.
-     * 
-     * log(x^a e^{-x}/(aΓ(a))) = alog(x) - x - log(a) - logΓ(a)
-     * = alog(x/a) - (x - a) - (1/2)log(a) - (1/2)log(2π) - f(a)
-     * f(a)はlogΓ(a)のスターリング近似の残差であり,
-     * f(a) = logΓ(a) - (a - 1/2)log(a) + a - (1/2)log(2π)
-     * である.
-     */
-    /**
-     * residualLogFactor = - (1/2)log(a) - (1/2)log(2π) - f(a)
-     */
-    private final double residualLogFactor;
+    private final CFracBasedIcgammaCalculator fractionCoefficient;
 
     /**
      * @param a パラメータ
@@ -63,8 +45,8 @@ final class ICGammaAtMiddleParam extends SkeletalICGamma {
         this.sqrtA = Exponentiation.sqrt(a);
         this.xLowerThreshold = a - this.sqrtA;
         this.xUpperThreshold = a + this.sqrtA;
-        this.residualLogFactor = -0.5 * Exponentiation.log(a) - HALF_LN2PI
-                - GammaFunction.lgammaStirlingResidual(a);
+
+        this.fractionCoefficient = CFracBasedIcgammaCalculator.of(a);
     }
 
     @Override
@@ -72,52 +54,21 @@ final class ICGammaAtMiddleParam extends SkeletalICGamma {
         final double thisA = this.a;
 
         if (x < this.xLowerThreshold) {
-            double lcp = ICGContinuedFractionFactor.factorLCP(x, thisA) * coeffToLCP(x);
+            double lcp = this.fractionCoefficient.calcP(x);
             return lcp / (1 - lcp);
         }
         if (x > this.xUpperThreshold) {
-            double ucp = ICGContinuedFractionFactor.factorUCP(x, thisA) * coeffToLCP(x) * (thisA / x);
+            double ucp = this.fractionCoefficient.calcQ(x);
             return (1 - ucp) / ucp;
         }
 
         if (x < thisA) {
             int shift = (int) (x - this.xLowerThreshold) + 1;
-            double lcp = factorLCPWithShift(x, thisA, shift) * coeffToLCP(x);
+            double lcp = this.fractionCoefficient.calcPByShift(x, shift);
             return lcp / (1 - lcp);
         }
         int shift = (int) (this.xUpperThreshold - x) + 1;
-        double ucp = factorUCPWithShift(x, thisA, shift) * coeffToLCP(x) * (thisA / x);
+        double ucp = this.fractionCoefficient.calcQByShift(x, shift);
         return (1 - ucp) / ucp;
-    }
-
-    private static double factorLCPWithShift(double x, double a, int shift) {
-        double out = ICGContinuedFractionFactor.factorLCP(x, a + shift);
-        for (int n = shift; n >= 1; n--) {
-            out *= x / (a + n);
-            out += 1;
-        }
-        return out;
-    }
-
-    private static double factorUCPWithShift(double x, double a, int shift) {
-        double out = ICGContinuedFractionFactor.factorUCP(x, a - shift);
-        double invX = 1 / x;
-        for (int n = shift; n >= 1; n--) {
-            out *= (a - n) * invX;
-            out += 1;
-        }
-        return out;
-    }
-
-    /**
-     * x^a e^{-x}/(aΓ(a))
-     */
-    private double coeffToLCP(double x) {
-        final double thisA = this.a;
-        final double logFactor = thisA * Exponentiation.log(x / thisA);
-        if (logFactor == Double.POSITIVE_INFINITY) {
-            return 0;
-        }
-        return Exponentiation.exp(logFactor - (x - thisA) + this.residualLogFactor);
     }
 }
